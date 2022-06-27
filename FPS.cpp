6d84a3396374a0b738e9618bb766f1028ec8103d
mateussangalli <mateussangalli@gmail.com>
2022-06-27 16:53:06 +0200

    do less comparisons between distances

--- FPS.cpp
+++ FPS.cpp
@@ -4,14 +4,6 @@
 
 namespace py = pybind11;
 
-//  struct buffer_info {
-//      void *ptr;
-//      py::ssize_t itemsize;
-//      std::string format;
-//      py::ssize_t ndim;
-//      std::vector<py::ssize_t> shape;
-//      std::vector<py::ssize_t> strides;
-//  };
 
 py::array_t<int> farthest_point_sampling(py::array_t<float> distance_matrix, int n_points) {
     py::buffer_info buf = distance_matrix.request();
@@ -38,6 +30,8 @@ py::array_t<int> farthest_point_sampling(py::array_t<float> distance_matrix, int
     ptr_out[0] = first_point;
 
     int remaining_points[N];
+    float remaining_dists[N];
+
     int n_rmng = N-1;
     int idx = 0;
     int v = 0;
@@ -51,28 +45,31 @@ py::array_t<int> farthest_point_sampling(py::array_t<float> distance_matrix, int
         }
     }
 
+    for (int idx = 0; idx < N; idx++) {
+        remaining_dists[idx] = 9999999.;
+    }
 
-    float min = 100000.f;
+
     float max = 0.f;
     int max_ind = 0;
-    float current_dist;
+    int last_ind = first_point;
 
     for (size_t idx_out = 1; idx_out < n_points; idx_out++) {
         max = 0.f;
         max_ind = -1;
         for (int idx_rmng = 0; idx_rmng < n_rmng; idx_rmng++) {
-            for (size_t idx = 0; idx < idx_out; idx++){
-                current_dist = ptr_in[N*ptr_out[idx] + idx_rmng];
-                min = 999999.f;
-                if (current_dist < min) min = current_dist;
+            if (ptr_in[N * remaining_points[idx_rmng] + last_ind] < remaining_dists[idx_rmng]) {
+                remaining_dists[idx_rmng] = ptr_in[N * idx_rmng + last_ind];
             }
-            if (min > max) {
-                max = min;
-                max_ind = idx_rmng;
+            if (remaining_dists[idx_rmng] > max) {
+                max = remaining_dists[idx_rmng];
+                max_ind = remaining_points[idx_rmng];
             }
         } 
-        remaining_points[max_ind] = remaining_points[n_rmng-1];
         n_rmng--;
+        remaining_points[max_ind] = remaining_points[n_rmng];
+        remaining_dists[max_ind] = remaining_dists[n_rmng];
+        last_ind = max_ind;
         ptr_out[idx_out] = max_ind;
     }
     return indices;
