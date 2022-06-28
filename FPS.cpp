6bf29d56d671b827bba0e67851b18c9897c2f03f
Mateus <mateussangalli@gmail.com>
2022-06-29 00:19:43 +0200

    optimize implemetation

--- FPS.cpp
+++ FPS.cpp
@@ -1,21 +1,50 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <iostream>
+#include <list>
+#include <queue>
 
 namespace py = pybind11;
 
+struct Center;
 
-py::array_t<int> farthest_point_sampling(py::array_t<float> distance_matrix, int n_points) {
-    py::buffer_info buf = distance_matrix.request();
-    int first_point = std::rand() % n_points; // choose a random point as seed
+struct ServedPoint {
+    float distance;
+    int index;
+    Center *parent;
+};
+
+struct CompareDistance {
+    bool operator()(ServedPoint const& a, ServedPoint const& b) {
+        return (a.distance < b.distance);
+    }
+};
+
+
+struct Center {
+    int index;
+    std::list<Center*> friends;
+    std::list<ServedPoint*> served_points;
+};
+
+
+float sqr_eucl_distance(float *a, float *b, int d) {
+    float out = 0.;
+    for (int idx = 0; idx < d; idx++) {
+        out += (a[idx] - b[idx]) * (a[idx] - b[idx]);
+    }
+    return out;
+}
+
+py::array_t<int> farthest_point_sampling(py::array_t<float> points, int n_points, int first_point) {
+    py::buffer_info buf = points.request();
 
     if (buf.ndim != 2)
         throw std::runtime_error("Number of dimensions must be two");
 
-    if (buf.shape[0] != buf.shape[1])
-        throw std::runtime_error("Distance matrix must be square");
 
     int N = buf.shape[0];
+    int d = buf.shape[1];
     if (N <= n_points) {
         throw std::runtime_error("Number of sampled points must be smaller than number of input points");
     }
@@ -29,50 +58,126 @@ py::array_t<int> farthest_point_sampling(py::array_t<float> distance_matrix, int
     int *ptr_out = static_cast<int *>(buf_out.ptr);
     ptr_out[0] = first_point;
 
-    int remaining_points[N];
-    float remaining_dists[N];
+    // initialize centers
+    std::vector<Center> centers(n_points);
+    for (int idx=0; idx < n_points; idx++) {
+        //Center *temp = (Center*)malloc(sizeof(Center));
+        Center c0;
+        std::list<ServedPoint*> temp1;
+        std::list<Center*> temp2;
+        c0.served_points = temp1;
+        c0.friends = temp2;
+        centers[idx] = c0;
+    }
+    centers[0].index = first_point;
+    centers[0].friends.push_back(&centers[0]);
 
-    int n_rmng = N-1;
-    int idx = 0;
-    int v = 0;
-    while (idx < n_rmng) {
-        if (v == first_point) {
-            v++;
-        }else {
-            remaining_points[idx] = v;
-            idx++;
-            v++;
+    float max_dist = 0.;
+    float dist;
+    // fursthest served point from the first point
+    ServedPoint *p0;
+    // initialize vector of served points by the first point
+    for (int idx = 0; idx < N; idx++) {
+        dist = sqr_eucl_distance(ptr_in + first_point * d, ptr_in + idx * d, d);  
+        ServedPoint* point = (ServedPoint*)malloc(sizeof(ServedPoint));
+        point->distance = dist;
+        point->index = idx;
+        point->parent = &centers[0];
+        if (dist > max_dist) {
+            p0 = point;
+            max_dist = dist;
+        }
+        centers[0].served_points.push_back(point);
+    }
+    std::priority_queue<ServedPoint, std::vector<ServedPoint>, CompareDistance> furthest_served_points;
+    furthest_served_points.push(*p0);
+
+
+    float r = 999999.;
+
+    std::list<Center*>::iterator it_friends;
+    std::list<ServedPoint*>::iterator it_sp;
+    float dist_new;
+    float dist_centers_old;
+    float dist_centers_new;
+    ServedPoint p, p1, p2;
+    Center *c_old;
+    for (int idx = 1; idx < n_points; idx ++) {
+        // find next center points
+        p = furthest_served_points.top();
+        furthest_served_points.pop();
+        ptr_out[idx] = p.index;
+        r = p.distance;
+
+        c_old = p.parent;
+
+
+        centers[idx].index = p.index;
+        centers[idx].friends.push_back(&centers[idx]);
+
+        // delete friends
+        it_friends = c_old->friends.begin();
+        while (it_friends != c_old->friends.end()) {
+            dist_centers_old = sqr_eucl_distance(ptr_in + (d * c_old->index), ptr_in + (d * ((*it_friends)->index)), d);
+            if (dist_centers_old > 64 * r) {
+                // it_friends->friends.erase(&c_old);
+                c_old->friends.erase(it_friends++);
+            }
+            else it_friends++;
+        }
+
+        // update served points
+        it_friends = c_old->friends.begin();
+        while (it_friends != c_old->friends.end()) {
+            it_sp = (*it_friends)->served_points.begin();
+            while(it_sp != (*it_friends)->served_points.end()) {
+                dist_new = sqr_eucl_distance(ptr_in + (d * p.index), ptr_in + (d * ((*it_sp)->index)), d);
+                if (dist_new < (*it_sp)->distance) {
+                    (*it_sp)->distance = dist_new;
+                    (*it_sp)->parent = &centers[idx];
+                    centers[idx].served_points.push_back(*it_sp);
+                    (*it_friends)->served_points.erase(it_sp++);
+                }else {
+                    it_sp++;
+                }
+            }
+            it_friends++;
+        }
+        // add friends
+        it_friends = c_old->friends.begin();
+        while (it_friends != c_old->friends.end()) {
+            dist_centers_new = sqr_eucl_distance(ptr_in + (d * p.index), ptr_in + (d * ((*it_friends)->index)), d);
+            if (dist_centers_new < 16 * r) {
+                centers[idx].friends.push_back(*it_friends);
+                (*it_friends)->friends.push_back(&centers[idx]);
+            }
+            it_friends++;
+        }
+
+        max_dist = 0.;
+        for (it_sp = c_old->served_points.begin(); it_sp != c_old->served_points.end(); it_sp++) {
+            if ((*it_sp) -> distance > max_dist){
+                p1 = **it_sp;
+                max_dist = (*it_sp) -> distance;
+            }
+        }
+        max_dist = 0.;
+        for (it_sp = centers[idx].served_points.begin(); it_sp != centers[idx].served_points.end(); it_sp++) {
+            if ((*it_sp) -> distance > max_dist){
+                p2 = **it_sp;
+                max_dist = (*it_sp) -> distance;
+            }
+        }
+        furthest_served_points.push(p1);
+        furthest_served_points.push(p2);
+    }
+    for (int idx=0; idx < n_points; idx++) {
+        // free(centers[idx]);
+        for (it_sp=centers[idx].served_points.begin(); it_sp != centers[idx].served_points.end(); it_sp++) {
+            free(*it_sp);
         }
     }
 
-    for (int idx = 0; idx < N; idx++) {
-        remaining_dists[idx] = 9999999.;
-    }
-
-
-    float max = 0.f;
-    int max_ind = 0;
-    int last_ind = first_point;
-
-    for (size_t idx_out = 1; idx_out < n_points; idx_out++) {
-        max = 0.f;
-        max_ind = -1;
-        for (int idx_rmng = 0; idx_rmng < n_rmng; idx_rmng++) {
-            // update distances array
-            if (ptr_in[N * remaining_points[idx_rmng] + last_ind] < remaining_dists[idx_rmng]) {
-                remaining_dists[idx_rmng] = ptr_in[N * idx_rmng + last_ind];
-            }
-            if (remaining_dists[idx_rmng] > max) {
-                max = remaining_dists[idx_rmng];
-                max_ind = remaining_points[idx_rmng];
-            }
-        } 
-        n_rmng--;
-        remaining_points[max_ind] = remaining_points[n_rmng];
-        remaining_dists[max_ind] = remaining_dists[n_rmng];
-        last_ind = max_ind;
-        ptr_out[idx_out] = max_ind;
-    }
     return indices;
 }
 
--- test_FPS.py
+++ test_FPS.py
@@ -2,11 +2,14 @@ import FPS
 import numpy as np
 import time
 
-print('testing using a 100 x 100 matrix') 
+print('5 points') 
 t0 = time.time()
-a = np.random.rand(100, 100)
+a = np.random.rand(5, 3)
+print(a)
+dist_matrix = np.sum((a[:,np.newaxis,:] - a[np.newaxis,:,:])**2, 2)
+print(dist_matrix)
 
-b = FPS.farthest_point_sampling(a, 30)
+b = FPS.farthest_point_sampling(a, 3, np.random.randint(0,4))
 
 print(f'output_shape: {b.shape}')
 print('indices:')
@@ -14,19 +17,26 @@ print(b)
 print(f'elapsed time: {time.time() - t0}')
 
 
-print('testing using a 10000 x 10000 matrix') 
+print('10000 points') 
 t0 = time.time()
-a = np.random.rand(10000, 10000)
+a = np.random.rand(10000, 3)
+dist_matrix1 = np.sum((a[:,np.newaxis,:] - a[np.newaxis,:,:])**2, 2)
 
-b = FPS.farthest_point_sampling(a, 1000)
-
+b = FPS.farthest_point_sampling(a, 2048, 42)
+b2 = FPS.farthest_point_sampling(a, 10, 42)
+print(f'perm1: {b[:10]}')
+print(f'perm2: {b2}')
+a2 = a[b, :]
+dist_matrix2 = np.sum((a2[:,np.newaxis,:] - a2[np.newaxis,:,:])**2, 2)
 print(f'output_shape: {b.shape}')
 print(f'elapsed time: {time.time() - t0}')
+print(f'original distance matrix mean: {dist_matrix1.mean()}')
+print(f'new distance matrix mean: {dist_matrix2.mean()}')
 
 
-print('testing using 10 10000 x 10000 matrix') 
+print('10 x 10000 points') 
 t0 = time.time()
 for i in range(10):
-    a = np.random.rand(10000, 10000)
-    b = FPS.farthest_point_sampling(a, 1000)
+    a = np.random.rand(10000, 3)
+    b = FPS.farthest_point_sampling(a, 2048, np.random.randint(0,10000))
 print(f'elapsed time: {time.time() - t0}')
